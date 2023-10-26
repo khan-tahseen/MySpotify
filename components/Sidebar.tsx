df84398d82ee278033792665d1523449d8204987
khan-tahseen <tasheenkhanit@gmail.com>
2023-10-26 21:40:04 +0530

    sidebar added and much more => spotify clone

---
+++ components/Sidebar.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { useMemo } from 'react';
+import { HiHome } from 'react-icons/hi';
+import { BiSearch } from 'react-icons/bi';
+import Box from './Box';
+import SidebarItem from './SidebarItem';
+import Library from './Library';
+
+interface SidebarProps {
+  children: React.ReactNode;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+  const pathName = usePathname();
+  const routes = useMemo(
+    () => [
+      {
+        icon: HiHome,
+        label: 'Home',
+        active: pathName !== '/search',
+        herf: '/',
+      },
+      {
+        icon: BiSearch,
+        label: 'Search',
+        active: pathName === '/search',
+        herf: '/search',
+      },
+    ],
+    [pathName]
+  );
+  return (
+    <div className="flex h-full">
+      <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+        <Box>
+          <div className="flex flex-col gap-y-4 px-4 py-2">
+            {routes.map((item) => (
+              <SidebarItem href={item.herf} key={item.label} {...item} />
+            ))}
+          </div>
+        </Box>
+        <Box className="overflow-y-auto h-full">
+          <Library />
+        </Box>
+      </div>
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
+    </div>
+  );
+};
+
+export default Sidebar;
