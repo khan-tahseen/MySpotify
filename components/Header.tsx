228d4ae38f6b2c80fe74170e9dba69c6d5867925
khan-tahseen <tasheenkhanit@gmail.com>
2023-10-27 09:52:48 +0530

    Custom added for logIn and signUp

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,9 @@
 import { useRouter } from 'next/navigation';
 import { twMerge } from 'tailwind-merge';
 import { RxCaretLeft, RxCaretRight } from 'react-icons/rx';
+import { HiHome } from 'react-icons/hi';
+import { BiSearch } from 'react-icons/bi';
+import Button from './Button';
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -37,7 +40,27 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             <RxCaretRight size={35} className="text-white" />
           </button>
         </div>
+
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-70 transition">
+            <HiHome size={20} className="text-black" />
+          </button>
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-70 transition">
+            <BiSearch size={20} className="text-black" />
+          </button>
+        </div>
+        <div className="flex items-center justify-center gap-x-4">
+          <>
+            <div>
+              <Button className='bg-transparent text-neutral-300 font-medium'>Sign Up</Button>
+            </div>
+            <div>
+              <Button className='bg-white px-6 py-2'>Log In</Button>
+            </div>
+          </>
+        </div>
       </div>
+      {children}
     </div>
   );
 };
