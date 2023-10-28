cddf0b34b8db35d6f85b9fbd2e40bf876e0423ba
khan-tahseen <tasheenkhanit@gmail.com>
2023-10-28 23:10:10 +0530

    List Item added home page for liked songs

---
+++ components/ListItem.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { FaPlay } from 'react-icons/fa';
+
+interface ListItemProps {
+  image: string;
+  name: string;
+  href: string;
+}
+
+const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    // Add authincation before push
+    router.push(href);
+  };
+
+  // if (!image && !name && !href) {
+  //   return null;
+  // }
+
+  return (
+    <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+      <div className="relative min-h-[64px] min-w-[64px]">
+        <Image className="object-cover" fill src={image} alt="image" />
+      </div>
+      <p className="font-medium truncate py-4">{name}</p>
+      <div className="absolute transition opacity-0 rounded-full flex items-center justify-center bg-green-500 p-4 drop-shadow-md hover:scale-110 right-5 group-hover:opacity-100">
+        <FaPlay className="text-black" />
+      </div>
+    </button>
+  );
+};
+
+export default ListItem;
