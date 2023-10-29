8f5b8e7366c5b10e3ca275d2a0ab6a2ed0f66469
khan-tahseen <tasheenkhanit@gmail.com>
2023-10-29 23:47:08 +0530

    userProvider done

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -76,3 +76,11 @@ export const MyUserContextProvider = (props: Props) => {
 
   return <UserContext.Provider value={value} {...props} />;
 };
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error('useUser must be used within a MyUserContextProvider');
+  }
+  return context;
+};
