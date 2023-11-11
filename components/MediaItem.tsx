24b5fd08554bd16e445d98ffd58fd2be2cf12dd8
khan-tahseen <tasheenkhanit@gmail.com>
2023-11-11 13:39:41 +0530

    working on media item

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import useLoadImage from '@/hooks/useLoadImage';
 import { Song } from '@/types';
+import Image from 'next/image';
 
 interface MediaItemProps {
   data: Song;
@@ -8,7 +10,31 @@ interface MediaItemProps {
 }
 
 const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
-  return <div className="media-item">this is media..</div>;
+  const imageUrl = useLoadImage(data);
+
+  const handleClick = () => {
+    if (onClick) {
+      onClick(data.id);
+    }
+
+    // TODO: Default turn on player
+  };
+  return (
+    <div
+      onClick={handleClick}
+      className="flex items-center gap-x-3 cursor-pointer hover:bg-neutral-800/50 w-full p-2 rounded-md"
+    >
+      this is media..
+      <div className="relative rounded-md min-h-[48px] min-w-[48px] overflow-hidden">
+        <Image
+          fill
+          src={imageUrl || '/images/liked.png'}
+          alt="Media Item"
+          className="object-cover"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default MediaItem;
