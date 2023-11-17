b640f6cebf6812122e8770804733fdba36fe17af
khan-tahseen <tasheenkhanit@gmail.com>
2023-11-18 03:28:57 +0530

    working on searcb input

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,7 +1,32 @@
 'use client';
 
+import qa from 'query-string';
+import useDebounce from '@/hooks/useDebounce';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import Input from './Input';
+
 const SearchInput = () => {
-  return <div className="search-input">Search Input..</div>;
+  const router = useRouter();
+  const [value, setValue] = useState<string>('');
+  const debouncedValue = useDebounce<string>(value, 500);
+
+  useEffect(() => {
+    const query = {
+      title: debouncedValue,
+    };
+
+    const url = qa.stringifyUrl({ url: '/search', query: query });
+    router.push(url);
+  }, [debouncedValue, router]);
+
+  return (
+    <Input
+      placeholder="What do you want to listen to ?"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
 };
 
 export default SearchInput;
