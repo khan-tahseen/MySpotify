72a79f94b8eaa39625d41a19f12706365330b25e
khan-tahseen <tasheenkhanit@gmail.com>
2023-11-18 18:33:56 +0530

    search is working now

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import MediaItem from '@/components/MediaItem';
 import { Song } from '@/types';
 
 interface SearchContentProps {
@@ -14,7 +15,17 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
       </div>
     );
   }
-  return <div className="search-content">Search Content...</div>;
+  return (
+    <div className="flex flex-col gap-y-2 w-full px-6">
+      {songs.map((song) => (
+        <div className="flex items-center gap-x-4 w-full" key={song.id}>
+          <div className="flex-1">
+            <MediaItem data={song} onClick={() => {}} />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default SearchContent;
