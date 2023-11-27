daa385e6ec6f03277e1564158835722bc7b9e8c7
khan-tahseen <tasheenkhanit@gmail.com>
2023-11-27 22:33:07 +0530

    working on liked page

--- app/liked/page.tsx
+++ app/liked/page.tsx
@@ -1,5 +1,22 @@
-const Liked = () => {
-  return <div>Liked page</div>;
+import getLikedSongs from '@/actions/getLikedSongs';
+import Header from '@/components/Header';
+import Image from 'next/image';
+
+export const revalidate = 0;
+
+const Liked = async () => {
+  const songs = await getLikedSongs();
+  return (
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+        <div className="flex flex-col md:flex-row items-center gap-x-5">
+          <div className="relative h-32 w-32 lg:h-44 lg:w-44">
+            <Image src={'/images/liked.png'} alt="Playlist" fill />
+          </div>
+        </div>
+      </Header>
+    </div>
+  );
 };
 
 export default Liked;
