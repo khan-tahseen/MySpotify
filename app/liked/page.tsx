db6276ce66048b79d9946d27b969fdc9a785d1a6
khan-tahseen <tasheenkhanit@gmail.com>
2023-12-07 20:02:29 +0530

    working on liked song playlist

--- app/liked/page.tsx
+++ app/liked/page.tsx
@@ -13,6 +13,12 @@ const Liked = async () => {
           <div className="relative h-32 w-32 lg:h-44 lg:w-44">
             <Image src={'/images/liked.png'} alt="Playlist" fill />
           </div>
+          <div className="flex flex-col gap-y-2 mt-4 md:mt-0">
+            <p className="hidden md:block font-semibold text-sm">Playlist</p>
+            <h1 className="text-white text-4xl sm:text-5xl lg:text-7xl font-bold">
+              Liked song
+            </h1>
+          </div>
         </div>
       </Header>
     </div>
