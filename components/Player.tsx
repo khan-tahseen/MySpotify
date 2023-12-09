b65e13e9b1e531ec09b58a0ae5113352e0b4b4db
khan-tahseen <tasheenkhanit@gmail.com>
2023-12-09 11:09:15 +0530

    useLoadSongUrl custom hook done

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,5 +1,21 @@
+'use client';
+
+import useGetSongById from '@/hooks/useGetSongById';
+import useLoadSongUrl from '@/hooks/useLoadSongUrl';
+import usePlayer from '@/hooks/usePlayer';
+
 const Player = () => {
-    return <div>Player..</div>
-}
+  const player = usePlayer();
+  const { song } = useGetSongById(player.activeId);
+  const songUrl = useLoadSongUrl(song!);
+
+  if (!song || !songUrl || !player.activeId) return null;
+
+  return (
+    <div className="fixed bottom-0 bg-black w-full py-2 px-4 h-[80px]">
+      Player..
+    </div>
+  );
+};
 
 export default Player;
