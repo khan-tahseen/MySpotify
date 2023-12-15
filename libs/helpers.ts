6fa515bccbba69a692fa8266e8d6270b4e028597
khan-tahseen <tasheenkhanit@gmail.com>
2023-12-15 23:21:02 +0530

    helper functions for stripe functionality

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -1,11 +1,37 @@
+import { Price } from "@/types";
+
 export const getUrls = () => {
-  let url =
-    process.env.NEXT_PUBLIC_SITE_URL ??
-    process.env.NEXT_PUBLIC_VERCEL_URL ??
-    'http://localhost:3000';
+    let url =
+        process.env.NEXT_PUBLIC_SITE_URL ??
+        process.env.NEXT_PUBLIC_VERCEL_URL ??
+        'http://localhost:3000';
 
-  url = url.includes('http') ? url : `https://${url}`;
-  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+    url = url.includes('http') ? url : `https://${url}`;
+    url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
 
-  return url;
+    return url;
 };
+
+export const postData = async ({ url, data }: { url: string, data?: { price: Price } }) => {
+    console.log('POST REQUEST => ', url, data);
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+        credentials: 'same-origin'
+    });
+
+    if (!response.ok) {
+        console.error('POST REQUEST ERROR => ', { url, data, response });
+        throw new Error(response.statusText);
+    }
+    return response.json();
+}
+
+export const toDateTime = (sec: number) => {
+    var t = new Date('1970-01-01T00:30:00Z');
+    t.setSeconds(sec);
+    return t;
+}
