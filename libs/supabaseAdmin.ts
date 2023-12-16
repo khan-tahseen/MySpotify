ca2d5e733a341e47beafaeffe48a528f81af2102
khan-tahseen <tasheenkhanit@gmail.com>
2023-12-16 12:50:54 +0530

    working on supabaseAdmin

--- libs/supabaseAdmin.ts
+++ libs/supabaseAdmin.ts
@@ -1,4 +1,4 @@
-import { Product } from '@/types';
+import { Price, Product } from '@/types';
 import { Database } from '@/types_db';
 import { createClient } from '@supabase/supabase-js';
 import Stripe from 'stripe';
@@ -17,4 +17,30 @@ const upsertProductRecord = async (product: Stripe.Product) => {
     active: product.active,
     metadata: product.metadata,
   };
+
+  const { error } = await supabaseAdmin.from('products').upsert(productData);
+  if (error) throw error;
+  console.log(`Product inserted/updated: ${product.id}`);
 };
+
+const upsertPriceRecord = async (price: Stripe.Price) => {
+  const priceData: Price = {
+    id: price.id,
+    product_id: typeof price.product === 'string' ? price.product : '',
+    active: price.active,
+    currency: price.currency,
+    description: price.nickname ?? undefined,
+    type: price.type,
+    unit_amount: price.unit_amount ?? undefined,
+    interval: price.recurring?.interval,
+    interval_count: price.recurring?.interval_count ?? undefined,
+    trial_period_days: price.recurring?.trial_period_days ?? undefined,
+    metadata: price.metadata,
+  };
+
+  const { error } = await supabaseAdmin.from('prices').upsert([priceData]);
+  if (error) throw error;
+  console.log(`Price inserted/updated: ${price.id}`);
+};
+
+
