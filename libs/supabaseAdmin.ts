0c8aded98e4d2e337315e0c61df51a471a352020
khan-tahseen <tasheenkhanit@gmail.com>
2023-12-16 10:47:04 +0530

    working on supabaseAdmin

--- libs/supabaseAdmin.ts
+++ libs/supabaseAdmin.ts
@@ -1,7 +1,20 @@
-import { Database } from "@/types_db";
-import { createClient } from "@supabase/supabase-js";
+import { Product } from '@/types';
+import { Database } from '@/types_db';
+import { createClient } from '@supabase/supabase-js';
+import Stripe from 'stripe';
 
 export const supabaseAdmin = createClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-    process.env.SUPABASE_SERVICE_ROLE_KEY || ''
-    )
+  process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+  process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+);
+
+const upsertProductRecord = async (product: Stripe.Product) => {
+  const productData: Product = {
+    id: product.id,
+    name: product.name,
+    description: product.description ?? undefined,
+    image: product.images?.[0] ?? null,
+    active: product.active,
+    metadata: product.metadata,
+  };
+};
