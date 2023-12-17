28e0afa9e67886a31aff4bcffba7fe8bf7552299
khan-tahseen <tasheenkhanit@gmail.com>
2023-12-17 15:44:23 +0530

    working on supabaseAdmin libs file

--- libs/supabaseAdmin.ts
+++ libs/supabaseAdmin.ts
@@ -85,5 +85,46 @@ const createOrRetrieveACustomer = async ({
     }
 
     console.log(`New customer created and inserted for ${uuid}`);
+    return cunstomer.id;
   }
+
+  return data?.stripe_customer_id;
+};
+
+const copyBillingDetailsToCustomer = async (
+  uuid: string,
+  payment_method: Stripe.PaymentMethod
+) => {
+  const customer = payment_method.customer as string;
+  const { name, phone, address } = payment_method.billing_details;
+
+  if (!name || !phone || !address) return;
+
+  // @ts-ignore
+  await stripe.customers.update(customer, { name, phone, address });
+  const { error } = await supabaseAdmin
+    .from('users')
+    .update({
+      billing_address: { ...address },
+      payment_method: { ...payment_method[payment_method.type] },
+    })
+    .eq('id', uuid);
+
+  if (error) throw error;
+};
+
+const manageSubscriptionStatusChange = async (
+  subscriptionId: string,
+  customerId: string,
+  createAction: false
+) => {
+  const { data: customerData, error: noCustomerError } = await supabaseAdmin
+    .from('customers')
+    .select('id')
+    .eq('stripe_customer_id', customerId)
+    .single();
+
+    if (noCustomerError) throw noCustomerError;
+
+    const {id: uuid} = customerData;
 };
