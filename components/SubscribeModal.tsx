f6abec8bf2d7b7061426a35e22dd365b4e7e139b
khan-tahseen <tasheenkhanit@gmail.com>
2024-01-04 00:54:08 +0530

    working on subscribeModal

--- components/SubscribeModal.tsx
+++ components/SubscribeModal.tsx
@@ -1,9 +1,34 @@
 'use client';
 
+import { ProductWithPrice } from '@/types';
 import Modal from './Modal';
 
-const SubscribeModal = () => {
+interface SubscribeModalProps {
+  products: ProductWithPrice[];
+}
+
+const SubscribeModal: React.FC<SubscribeModalProps> = ({ products }) => {
   let content = <div className="text-center">No Products available</div>;
+
+  if (products?.length) {
+    content = (
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {products.map((product) => {
+          if (!product.prices?.length) {
+            return (
+              <div
+                key={product.id}
+                className="bg-white rounded-lg shadow-md overflow-hidden"
+              >
+                {' '}
+                No prices available
+              </div>
+            );
+          }
+        })}
+      </div>
+    );
+  }
   return (
     <Modal
       title="Only for premium users"
